9464b4158a333faf777b938edb5e3dfdbf4373b3
Nasir-Kasumu <naskas1234@gmail.com>
2020-07-20 12:19:07 -0400

    added pdf analysis and fixed frequency of word glitch

--- BookParser.java
+++ BookParser.java
@@ -35,9 +35,8 @@ public class BookParser{
     HashMap<String, HashMap<String,Integer>> map = parseWords(file, sc); // key will be chapter value will be a map key = word, value = frequency of word
     HashSet<String> commonWords = new HashSet<String>();
     String chunk = "";
-    public BookParser() throws FileNotFoundException{
+    public BookParser() throws FileNotFoundException{ // override default method to parse frequentwords.txt and add to a set for later use
         File f = new File("frequentwords.txt");
-        //File f2 = new File("pride.txt");
         Scanner s = new Scanner(f);
         int count = 0;
         while(s.hasNext() && count < 100){
@@ -46,18 +45,9 @@ public class BookParser{
             count++;
         }
         s.close();
-        //s = new Scanner(f2);
-        //while(s.hasNext()){
-        //    String res = s.next();
-        //    chunk += res;
-        //}
-        //s.close();
     }
 
-    public void fillCommonwords(){
-
-    }
-    public PriorityQueue<String []> fillQ(HashMap<String,HashMap<String,Integer>> map, String type){ // fills the queue according to the map of prepopulated values
+    public PriorityQueue<String []> fillQ(HashMap<String,HashMap<String,Integer>> map, String type){ // fills the queue as a min or max heap for top 20 or least 20 values
         PriorityQueue<String[]> freqQ;
         if(type.equals("max")){
             freqQ = new PriorityQueue<String[]>(new maxHeap());;
@@ -82,7 +72,7 @@ public class BookParser{
         return freqQ;
     }
 
-    public static HashMap<String, HashMap<String,Integer>> parseWords(File f, Scanner sc){
+    public static HashMap<String, HashMap<String,Integer>> parseWords(File f, Scanner sc){ // fills out a hashmap with (key : (key2:value)) pairs where key is the chapter and key2:value is the word and frequency in the chapter
         HashMap<String,HashMap<String,Integer>> map = new HashMap<String,HashMap<String,Integer>>();
         String currentChapter = "0";
         while(sc.hasNextLine()){
@@ -99,7 +89,6 @@ public class BookParser{
                     
                     if(!currString.isBlank() && map.get(currentChapter) != null && map.get(currentChapter).containsKey(currString)){
                         HashMap<String,Integer> temp = map.get(currentChapter);
-                        //System.out.println("increment");
                         temp.put(currString, temp.get(currString) + 1);
                         map.put(currentChapter, temp);
                     }else if(currString != null && !currString.isBlank()){
@@ -113,7 +102,7 @@ public class BookParser{
         return map;
     }
 
-    public int getTotalNumberOfWords(){
+    public int getTotalNumberOfWords(){ //traverses through map adding up the values on the key value pairs and returns the sum
         int sum = 0;
         for(String x : map.keySet()){
             HashMap<String,Integer> curr = map.get(x);
@@ -124,7 +113,7 @@ public class BookParser{
         return sum;
     }
 
-    public int getTotalUniqueWords(){
+    public int getTotalUniqueWords(){  // traverse through map checking against common words hashset and returns values not in common words
         HashSet<String> uniques = new HashSet<String>();
         for(String x: map.keySet()){
             HashMap<String,Integer> curr = map.get(x);
@@ -135,7 +124,7 @@ public class BookParser{
         return uniques.size();
     }
 
-    public String [] [] get20MostFrequentWords(){
+    public String [] [] get20MostFrequentWords(){ // creates a max heap and returns the 20 top values
         String [] [] res = new String [20] [2];
         PriorityQueue<String[]> freqQ = fillQ(map,"max");
         for(int i = 0; i < 20; i++){
@@ -147,7 +136,7 @@ public class BookParser{
     }
 
 
-    public String [] [] get20MostInterestingFrequentWords(){
+    public String [] [] get20MostInterestingFrequentWords(){ // creates a max heap and returns the 20 top values ignoring those that are in the common words hashset
         String [] [] res = new String [20] [2];
         PriorityQueue<String []> freqQ = fillQ(map,"max");
         int count = 0;
@@ -162,7 +151,7 @@ public class BookParser{
         return res;
     }
 
-    public String [] [] get20LeastFrequentWords(){
+    public String [] [] get20LeastFrequentWords(){ //creates a min heap and returns the 20 top values 
         String [] [] res = new String [20] [2];
         PriorityQueue<String[]> freqQ = fillQ(map,"min");
         for(int i = 0; i < 20; i++){
@@ -173,18 +162,22 @@ public class BookParser{
         return res;
     }
 
-    public int [] getFrequencyOfWord(String word){
+    public int [] getFrequencyOfWord(String word){ // takes word as input and returns an array of size 61 there are 61 chapters of each words frequency
         int [] res = new int [61];
         int count = 0;
         for(String x : map.keySet()){
             HashMap<String,Integer> curr = map.get(x);
-            res[count] = curr.get(word);
+            if(curr.get(word) != null){
+                res[count] = curr.get(word);
+            }else{
+                res[count] = 0;
+            }
             count++;
         }
         return res;
     }
 
-    public String getChapterQuoteAppears(String quote) throws FileNotFoundException{ // ridiculously bad runtime
+    public String getChapterQuoteAppears(String quote) throws FileNotFoundException{ // splits by chapter into an array and returns value where quote appears
         File f2 = new File("pride.txt");
         Scanner s = new Scanner(f2);
         while(s.hasNext()){
@@ -206,7 +199,7 @@ public class BookParser{
                 }
             }
         }
-        return "String does not exist";
+        return "Quote does not exist";
     }
 /*
     public int generateSentence(){
@@ -220,8 +213,8 @@ public class BookParser{
         //System.out.println(b.getTotalNumberOfWords());
         //System.out.println(Arrays.deepToString(b.get20MostFrequentWords()));
         //System.out.println(Arrays.deepToString(b.get20MostInterestingFrequentWords()));
-        //System.out.println(Arrays.toString(b.getFrequencyOfWord("the")));
+        System.out.println(Arrays.toString(b.getFrequencyOfWord("elizabeth")));
         //System.out.println(Arrays.deepToString(b.get20LeastFrequentWords()));
-        //System.out.println(b.getChapterQuoteAppears("But to live in ignorance on such a point was impossible; or at least it was impossible not to try for information."));
+        //System.out.println(b.getChapterQuoteAppears("There are few people whom I really love, and still fewer of whom I think well"));
     }
 }
